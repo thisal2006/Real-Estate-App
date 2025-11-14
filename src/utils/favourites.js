071c5e03613b202e2011e5bb73cf8cbc0e38e18d
thisal sandayuru <thisal.20231581@iit.ac.lk>
2025-11-14 11:20:00 +0530

    Implement favourites utility with add/remove/clear and notifications

---
+++ src/utils/favourites.js
@@ -0,0 +1,35 @@
+import { toast } from 'react-toastify';
+
+let favourites = [];
+let listeners = [];
+
+const notify = () => listeners.forEach(l => l());
+
+export const add = (property) => {
+  if (!favourites.some(f => f.id === property.id)) {
+    favourites.push(property);
+    notify();
+    toast.success("Added to favorites!");
+  }
+};
+
+export const remove = (id) => {
+  favourites = favourites.filter(f => f.id !== id);
+  notify();
+  toast.info("Removed from favorites");
+};
+
+export const clear = () => {
+  favourites = [];
+  notify();
+  toast.info("Favorites cleared");
+};
+
+export const get = () => favourites;
+
+export const subscribe = (listener) => {
+  listeners.push(listener);
+  return () => { listeners = listeners.filter(l => l !== listener); };
+};
+
+export default { add, remove, clear, get, subscribe };
